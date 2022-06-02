34c68a60a724a4391c811738b2ddf5aae8ff5f46
leotis buchanan <leotisbuchanan@leotiss-MacBook-Air.local>
2022-06-02 12:10:21 -0400

    complete simple api

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import * as apiHooks from './lib/api-hooks';
+import { FetchState } from './types';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  const renderComponent = () => render(<App />);
+  const defaultText =
+    'Hello there, click the button below to get the list of posts from the API.';
+  const loadingText = 'Fetching posts...';
+  const errorText = 'Oops! Something went wrong. Please try again.';
+  const successText = "Here's the list of posts:";
+
+  it('should render default state', () => {
+    renderComponent();
+
+    const stateEl = screen.queryByText(defaultText);
+
+    expect(stateEl).toBeInTheDocument();
+  });
+
+  it('should render error state on error', () => {
+    const fetchState = FetchState.ERROR;
+
+    jest
+      .spyOn(apiHooks, 'useGetPosts')
+      .mockReturnValue([[], fetchState, jest.fn()]);
+
+    renderComponent();
+
+    const stateEl = screen.queryByText(errorText);
+
+    expect(stateEl).toBeInTheDocument();
+  });
 });
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useGetPosts} from './lib/api-hooks'; 
+import { FetchState } from './types';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+   const [posts, fetchState, getPosts] = useGetPosts(); 
+
+
+   return (
+
+    <div className="container">
+      <h1>React TypeScript API Hooks</h1>
+      {fetchState === FetchState.DEFAULT && (
+       <>
+         <p> 
+           Hello There, click the button below to get the list of posts from the API. 
+         </p>
+
+         <button onClick={getPosts}>Get Posts</button>
+       
+       
+       </>
+
+
+      )}
+
+      {fetchState === FetchState.LOADING &&  <p>Fetching posts....</p>}
+      {fetchState === FetchState.ERROR && (
+       
+       <>
+       <p>Oops! Something went wrong. Please try again.</p>
+       <button onClick={getPosts}>Get Posts</button>
+     </>
+
+      )}
+
+       {fetchState === FetchState.SUCCESS && (
+        <>
+          <p>Here's the list of posts:</p>
+          <ul className="posts-list">
+            {posts.map((post) => (
+              <li key={post.id} className="post">
+                <h3>
+                  {post.id} - {post.title}
+                </h3>
+                <p>{post.body}</p>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+
     </div>
-  );
-}
+
+       
+
+      
+         
+   ); 
+
+  }
+   
+  
+
 
 export default App;
